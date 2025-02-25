6865691ce1f84f3de3e4047861cf5fd211e13847
Voltline <44248193@qq.com>
2025-02-25 14:23:38 +0800

    [Feature]: 增加ColorLogger，Message解析，Config解析，TCP报文拆包等基础设施，等待所有基础设施完毕后完成服务器主体

--- Utils/config.go
+++ Utils/config.go
@@ -29,3 +29,28 @@ func NewConfig(filePath string) (*Config, error) {
 	}
 	return &config, nil
 }
+
+type COSConfig struct {
+	SecretID  string `json:"secret_id"`
+	SecretKey string `json:"secret_key"`
+	Region    string `json:"region"`
+}
+
+func NewCOSConfig(filePath string) (*COSConfig, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	var cos COSConfig
+	if err := json.Unmarshal(data, &cos); err != nil {
+		return nil, err
+	}
+	return &cos, nil
+}
--- main.go
+++ main.go
@@ -1,19 +1,75 @@
 package main
 
 import (
+	"Betterfly-Server-Go/Database"
 	"Betterfly-Server-Go/Utils"
-	"fmt"
+	"time"
 )
 
-func main() {
-	fmt.Println(Utils.Login)
+func test() {
+	logger := Utils.NewLogger()
+	logger.Info.Println(Utils.Login)
 	config, err := Utils.NewConfig("./Config/config.json")
 	if err != nil {
-		fmt.Println(err)
+		logger.Error.Println(err)
 	} else {
-		fmt.Println(config)
+		logger.Info.Println(config)
 	}
 
+	cosconfig, err := Utils.NewCOSConfig("./Config/cos_config.json")
+	if err != nil {
+		logger.Error.Println(err)
+	} else {
+		logger.Info.Println(cosconfig)
+	}
+
+	dbsetting, err := Database.NewDBSetting("./Config/database_config.json")
+	if err != nil {
+		logger.Error.Println(err)
+	} else {
+		logger.Info.Println(dbsetting)
+	}
+
+	logger.Info.Println(time.Now())
+
+	msg := "{\"type\":123,\n\"from\": 10213901414,\n\"name\": \"Voltline\",\n\"timestamp\":\"2025-02-25 10:20:34\"}"
+	logger.Info.Println(msg)
+	rmsg, err := Utils.NewRequestMessage(msg)
+	if err != nil {
+		logger.Error.Println(err)
+	} else {
+		logger.Info.Println(rmsg)
+	}
+
+	str1, err := Utils.MakeHelloMessage(123, 456, "Voltline", false, "")
+	if err != nil {
+		logger.Error.Println(err)
+	} else {
+		logger.Info.Println(str1)
+	}
+
+	logger.Info.Println("----Encoding and Decoding----")
+	s1 := "Hello, world!"
+	s2 := "Good world!"
+	s3, s4 := string(Utils.Encode(s1)), string(Utils.Encode(s2))
+	logger.Info.Println(s3, s4)
+	s5 := []byte(s3 + s4)
+	logger.Info.Printf("%q\n", Utils.Decode(s5))
+	message, err := Utils.MakeDownloadMessage("qq.dmg", "aaa")
+	if err != nil {
+		logger.Error.Println(err)
+	} else {
+		logger.Info.Println(string(Utils.Encode(message)))
+		logger.Info.Println(message)
+		logger.Info.Println(Utils.Decode(Utils.Encode(message)))
+	}
+}
+
+func main() {
+	test()
+	logger := Utils.NewLogger()
+	logger.Info.Println("Hello World")
+	logger.Error.Println("Error!")
 	//wp := Utils.NewWorkerPool(10, func(conn int) {
 	//	fmt.Println(conn)
 	//})

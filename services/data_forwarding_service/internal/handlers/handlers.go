4155b46ed831e7511388a40b7bffb476fbbaa9f3
Voltline <44248193@qq.com>
2025-03-30 16:17:41 +0800

    refactor: 换用zap和kafka消息队列，目前所有内容还未变更，待后续更改

--- services/data_forwarding_service/internal/handlers/handlers.go
+++ services/data_forwarding_service/internal/handlers/handlers.go
@@ -1,15 +1,13 @@
 package handlers
 
 import (
-	"data_forwarding_service/internal/logger"
 	"data_forwarding_service/internal/publisher"
 	"fmt"
 	"github.com/gorilla/websocket"
+	"go.uber.org/zap"
 	"net/http"
 	"sync"
 )
-
-var log = logger.NewLogger()
 
 // Client 连接管理
 type Client struct {
@@ -37,9 +35,12 @@ func StartWebSocketServer() error {
 
 // 请求处理
 func handleConnection(w http.ResponseWriter, r *http.Request) {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+	sugar := logger.Sugar()
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Error.Println(err)
+		sugar.Errorf("连接错误: %s\n", err)
 		return
 	}
 
@@ -56,12 +57,12 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 
 	// 存储连接
 	clientsMutex.Lock()
-	log.Info.Printf("保存连接 %v : %v\n", userID, *client)
+	sugar.Infof("保存连接 %v, %v\n", userID, *client)
 	clients[userID] = client
 	clientsMutex.Unlock()
 
-	log.Info.Printf("已与 %v 建立连接\n", conn.RemoteAddr())
-	log.Info.Printf("收到的Request内容为: %v\n", *r)
+	sugar.Infof("已与 %v 简历连接\n", conn.RemoteAddr())
+	sugar.Infof("收到的Request内容为: %v\n", *r)
 
 	// 启动读取处理和发送消息两个协程
 	// 读取处理协程
@@ -72,19 +73,22 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 
 // 读取处理协程
 func readProcess(client *Client, userID string) {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+	sugar := logger.Sugar()
 	defer func() {
 		clientsMutex.Lock()
 		delete(clients, userID)
 		clientsMutex.Unlock()
 		client.conn.Close()
-		log.Info.Printf("(%v, %v)连接已关闭\n", userID, client.conn.RemoteAddr())
+		sugar.Infof("(%v, %v)连接已关闭\n", userID, client.conn.RemoteAddr())
 	}()
 
 	for {
 		// 处理消息接收与转发
 		_, p, err := client.conn.ReadMessage()
 		if err != nil {
-			log.Error.Println("获取信息异常: ", err)
+			sugar.Errorln("获取信息异常: ", err)
 			break
 		}
 		if len(p) == 0 {
@@ -93,21 +97,24 @@ func readProcess(client *Client, userID string) {
 		}
 		err = publishMessage(p) // 将消息转发到消息队列
 		if err != nil {
-			log.Error.Println("转发信息到消息队列异常: ", err)
+			sugar.Errorln("转发信息到消息队列异常: ", err)
 			break
 		}
 		// TODO: DEBUG模式
-		log.Info.Println("收到WebSocket消息:", string(p))
+		sugar.Infoln("收到WebSocket消息:", string(p))
 	}
 }
 
 // 监听 channel 发送消息协程
 func writeToClient(client *Client, userID string) {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+	sugar := logger.Sugar()
 	defer client.conn.Close()
 	for msg := range client.sendChan {
 		err := client.conn.WriteMessage(websocket.TextMessage, msg)
 		if err != nil {
-			log.Error.Printf("发送消息错误: %v\n", err)
+			sugar.Errorln("发送消息错误: ", err)
 		}
 	}
 }

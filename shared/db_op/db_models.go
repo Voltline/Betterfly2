51e33d90e365f2a367352e48304ad72ef4345420
Voltline <44248193@qq.com>
2025-04-06 21:20:03 +0800

    feat: 修改User模型定义，增加Salt字段防止彩虹表攻击

--- shared/db_op/db_models.go
+++ shared/db_op/db_models.go
@@ -7,4 +7,5 @@ type User struct {
 	UpdateTime   string `gorm:"comment:上次更新个人资料时间，用于用户间的个人资料同步"`
 	Avatar       string `gorm:"comment:用户头像的url，图片存在COS或别的http服务器上"`
 	PasswordHash string `gorm:"comment:加密后的用户密码哈希值"`
+	Salt         string `gorm:"comment:每个用户不同的盐值，防止彩虹表攻击"`
 }

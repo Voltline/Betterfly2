c88d394ced52ff016e1db86da032b2e52e13cfd3
D_S_O_ <lty20021010@icloud.com>
2025-05-04 03:20:10 +0800

    fix: docker compose 构建时在后台运行并且强制重新构建镜像

--- services/build_docker_compose.sh
+++ services/build_docker_compose.sh
@@ -2,4 +2,4 @@ set -e  # 让脚本在任意命令失败时立即退出
 
 chmod 764 ../tool/bin/*
 make -C ../proto
-sudo docker compose up --build --remove-orphans
+sudo docker compose up -d --build --remove-orphans --force-recreate

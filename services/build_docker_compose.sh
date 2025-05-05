50166de36668da4fadae7b0a70c1b27aefec1db9
D_S_O_ <lty20021010@icloud.com>
2025-05-06 01:03:56 +0800

    fix: dockerfile设置go mod代理

--- services/build_docker_compose.sh
+++ services/build_docker_compose.sh
@@ -4,6 +4,6 @@ set -e  # 让脚本在任意命令失败时立即退出
 chmod +x ../common/ws_ssl/generate_self_signed_cert.sh
 ./../common/ws_ssl/generate_self_signed_cert.sh
 
-chmod 764 ../tool/bin/*
+chmod +x ../tool/bin/*
 make -C ../proto
 sudo docker compose up -d --build --remove-orphans --force-recreate

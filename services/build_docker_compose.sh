16b462140ea61d2fdb02e76d94b7a113e16ee668
Voltline <44248193@qq.com>
2025-05-06 10:37:58 +0800

    feat: 为所有可执行文件增加执行权限，去除chmod +x

--- services/build_docker_compose.sh
+++ services/build_docker_compose.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e  # 让脚本在任意命令失败时立即退出
 
-chmod +x ../common/ws_ssl/generate_self_signed_cert.sh
 ./../common/ws_ssl/generate_self_signed_cert.sh
 
-chmod +x ../tool/bin/*
 make -C ../proto
 sudo docker compose up -d --build --remove-orphans --force-recreate
